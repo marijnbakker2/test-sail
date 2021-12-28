3d0c228ee50285d9750db302d805392d672551f0
marijnbakker <marijnnbakker@gmail.com>
2021-12-29 00:17:01 +0100

    add random user to test route for db testing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,6 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function () {
-    return 'hello world';
+    $u = User::inRandomOrder()->first();
+    return 'Hi '.$u->name;
 });
